613796b8499de6d43360db6e9b65e05220f2b5fa
HakanYilandil <47758342+HakanYilandil@users.noreply.github.com>
2019-04-16 04:15:40 +0300

    Update ultrosonik.ino

--- ultrosonik.ino
+++ ultrosonik.ino
@@ -1,45 +1,50 @@
-const int trigger_pin = 7;
-const int echo_pin = 6;
-const int buzzer = 8;
-int sure ;
-int mesafe ;
-  
-void setup() {
+#define echoPin 6
+#define trigPin 7
+#define buzzerPin 8
 
-pinMode(buzzer , OUTPUT);
-pinMode(trigger_pin , OUTPUT);
-pinMode(echo_pin ,INPUT);
-Serial.begin(9600);
+int maximumRange = 50;
+int minimumRange = 0;
+
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+}
+
+void loop() {
+
+  int olcum = mesafe(maximumRange, minimumRange);
+  melodi(olcum*10);
+
+}
+
+int mesafe(int maxrange, int minrange)
+{
+  long duration, distance;
+
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 58.2;
+  delay(50);
+
+  if(distance >= maxrange || distance <= minrange)
+  return 0;
+  return distance;
+}
+
+int melodi(int dly)
+{
+  tone(buzzerPin, 440);
+  delay(dly);
+  noTone(buzzerPin);
+  delay(dly);
   
-}
-  
-void loop()
-{
-digitalWrite(trigger_pin , HIGH);
-delayMicroseconds(1000);
-digitalWrite(trigger_pin , LOW);
-sure = pulseIn(echo_pin , HIGH);
-mesafe = (sure/2) / 29.1;
-  
-if (mesafe <= 10)
-{
-digitalWrite(buzzer , HIGH);
-delay(250);
-digitalWrite(buzzer , LOW);
-}
-else if (mesafe <=25)
-{
-digitalWrite(buzzer , HIGH);
-delay(500);
-digitalWrite(buzzer , LOW);
-}
-else if ( mesafe <=50)
-{
-digitalWrite(buzzer , HIGH);
-delay(750);
-digitalWrite(buzzer , LOW);
-}
-else
-{
-  }
 }
